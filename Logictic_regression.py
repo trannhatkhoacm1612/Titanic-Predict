e1de78a61cb7d626729b140784c7e59e5106a4ce
Trần Nhật Khoa ( Woffy ) <120149585+trannhatkhoacm1612@users.noreply.github.com>
2023-01-18 22:12:20 +0700

    Update Logictic_regression.py

--- Logictic_regression.py
+++ Logictic_regression.py
@@ -3,19 +3,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-# Generate some fake data
-X = np.random.rand(100,2)
+x = np.random.rand(100,2)
 y = np.random.randint(0,2,100)
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+x_train, x_test, y_train,y_test = train_test_split(x,y,test_size=0.2)
 
-# Initialize and fit the model
-clf = LogisticRegression().fit(X_train, y_train)
+log = LogisticRegression()
+log.fit(x_train,y_train)
 
-# Make predictions on the test set
-y_pred = clf.predict(X_test)
+y_pre = log.predict(x_test)
 
-# Calculate the accuracy of the predictions
-acc = accuracy_score(y_test, y_pred)
-print("Accuracy:", acc)
+print('Acurancy: ',accuracy_score(y_test,y_pre))
